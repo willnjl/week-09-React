112e1decf48ef92e670925e949f28eeca52ce3fd
Will <will_leighton@outlook.com>
2020-08-24 14:27:14 +0100

    init

---
+++ day-1/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import Header from './components/Header.js';
+import './App.css';
+
+function App() {
+  return(
+    <Header />
+  );
+}
+
+export default App;
---
+++ day-1/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+    <>
+    <header>
+        <h1>
+            Hello, World!
+        </h1>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quamquam tu hanc copiosiorem etiam soles dicere. Ubi ut eam caperet aut quando? Videmus igitur ut conquiescere ne infantes quidem possint. Magna laus. Bonum patria: miserum exilium. Sed tu istuc dixti bene Latine, parum plane. Duo Reges: constructio interrete. Ergo hoc quidem apparet, nos ad agendum esse natos.
+        </p>
+    </header>
+    </>
+}
+
+export default Header;
