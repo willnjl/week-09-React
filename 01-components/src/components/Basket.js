d49c879acb1bdce05959b938c149f7568df7c88e
Will <will_leighton@outlook.com>
2020-08-24 20:48:04 +0100

    tricksy pre commit

--- 01-components/src/components/Basket.js
+++ 01-components/src/components/Basket.js
@@ -2,22 +2,24 @@ import React from "react";
 
 const Basket = ({ items }) => (
   <>
-    <table className="table-bordered">
-      <thead>
-        <tr>
-          <th scope="col">name</th>
-          <th scope="col">price</th>
-        </tr>
-      </thead>
-      <tbody>
-        {items.map((item, i) => (
-          <tr key={i} scope="row">
-            <td>{item.name}</td>
-            <td>{item.price}</td>
+    <div className="container-sm">
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">name</th>
+            <th scope="col">price</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {items.map((item, i) => (
+            <tr key={i} scope="row">
+              <td>{item.name}</td>
+              <td>{item.price}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   </>
 );
 export default Basket;
