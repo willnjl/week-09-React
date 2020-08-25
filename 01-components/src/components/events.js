11a42e57ad72477dfe5287b14c528deb46745c5f
Will <will_leighton@outlook.com>
2020-08-25 16:12:36 +0100

    Catch Me Challenge Completed

--- 01-components/src/components/events.js
+++ 01-components/src/components/events.js
@@ -2,12 +2,14 @@ import Clicked from "./events/Clicked";
 import Square from "./events/Square";
 import ToggleText from "./events/ToggleText";
 import StepCounter from "./events/StepCounter";
+import CatchMeIfYoCan from "./events/CatchMeIfYouCan";
 
 import React from "react";
 const Events = () => (
 	<>
 		<div className="">
 			<Clicked />
+			<CatchMeIfYoCan jump={50} />
 			<Square colour={"hotpink"} />
 			<ToggleText initial="Hello" alternate="World!" />
 			<StepCounter max={100} step={5} />
