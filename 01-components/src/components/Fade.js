0d8ff58d4cd07b2d581fbf37c8f26b14d9d4e7d5
Will <will_leighton@outlook.com>
2020-08-25 11:41:32 +0100

    components challenges completed

--- 01-components/src/components/Fade.js
+++ 01-components/src/components/Fade.js
@@ -1,15 +1,17 @@
 import React from "react";
 
 const Fade = ({ time, children }) => (
-  <>
-    <div
-      style={{
-        animationName: "fade",
-        animationDuration: time,
-      }}
-    >
-      {children}
-    </div>
-  </>
+  <div
+    style={{
+      animationName: "fade",
+      animationDuration: time,
+    }}
+  >
+    {children}
+  </div>
 );
+
+Fade.defaultProps = {
+  time: "3s",
+};
 export default Fade;
