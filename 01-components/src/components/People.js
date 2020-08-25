5f698a6cfe87124d6cdfcc2a43234c923bf92a54
Will <will_leighton@outlook.com>
2020-08-25 12:54:01 +0100

    02 completed

--- 01-components/src/components/People.js
+++ 01-components/src/components/People.js
@@ -1,24 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
 
-const People = ({ names }) => (
-  <>
-    <section className="list-group">
-      <ul>
-        {!names ? (
-          <li className="alert alert-warning" key={0}>
-            {" "}
-            "Nothing To See Here"
-          </li>
-        ) : (
-          names.map((name, index) => (
-            <li key={index} className="list-group-item">
-              {name}
-            </li>
-          ))
-        )}
-      </ul>
-    </section>
-  </>
-);
+class People extends Component {
+	render() {
+		const { names } = this.props;
+		return (
+			<section className="list-group">
+				<ul onClick={() => console.log("Hello, World!")}>
+					{!names ? (
+						<li className="alert alert-warning" key={0}>
+							{" "}
+							"Nothing To See Here"
+						</li>
+					) : (
+						names.map((name, index) => (
+							<li key={index} className="list-group-item">
+								{name}
+							</li>
+						))
+					)}
+				</ul>
+			</section>
+		);
+	}
+}
 
 export default People;
