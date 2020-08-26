604511155eff541fe5fc4ae41738569d5efb0d78
Will <will_leighton@outlook.com>
2020-08-26 15:51:16 +0100

    challenge Temp completed

--- 01-components/src/components/forms/Forms.js
+++ 01-components/src/components/forms/Forms.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Length from "./Length";
 import Password from "./Password";
+import TempConverter from "./TempConverter";
 
 export default function Forms() {
 	return (
 		<>
 			<Length />
 			<Password />
+			<TempConverter />
 		</>
 	);
 }
