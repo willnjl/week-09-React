8099bade14ebe65eaaed0e74552c5d2617a57bea
Will <will_leighton@outlook.com>
2020-08-27 17:07:30 +0100

    catch me challenge completed

---
+++ 01-components/src/components/prebuilt/Prebuilt.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CatchMe from "./CatchMe";
+import ProgressChallenge from "./ProgressChallenge";
+
+export default function Prebuilt() {
+	return (
+		<div>
+			<CatchMe />
+			<ProgressChallenge />
+		</div>
+	);
+}
---
+++ 01-components/src/components/prebuilt/ProgressChallenge.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import Button from "react-bootstrap/Button";
+import Progress from "react-bootstrap/ProgressBar";
+
+export default class ProgressChallenge extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			val: 0,
+		};
+		this.handleClick = this.handleClick.bind(this);
+	}
+
+	handleClick(e) {
+		this.setState({
+			val: this.state.val + 10,
+		});
+	}
+	render() {
+		return (
+			<div style={{ margin: "100px auto", width: "50%" }}>
+				<Progress now={this.state.val} />
+				<Button onClick={(e) => this.handleClick(e)}>Button</Button>
+			</div>
+		);
+	}
+}
