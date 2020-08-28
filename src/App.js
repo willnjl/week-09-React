de0ecbb978670ec141e6a97383152269d8bdf78d
Will <will_leighton@outlook.com>
2020-08-28 12:03:28 +0100

    git hub view deployment change

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import Header from "./components/Header";
 // import logo from './logo.svg';
 import Stuff from "./components/Stuff";
