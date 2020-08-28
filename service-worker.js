aa6ffa3f4f3a8d5c83dc410000c39e4fc81ad598
Will <will_leighton@outlook.com>
2020-08-28 11:53:26 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/https://github.com/willnjl/week-09-React/tree/master/01-components/precache-manifest.98391d2ebc38d6fcd51e9bd47345e86d.js"
+  "/week-09-React/tree/master/01-components/precache-manifest.77661b8a54ec61524453eefe0b6a1868.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/https://github.com/willnjl/week-09-React/tree/master/01-components/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/week-09-React/tree/master/01-components/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
