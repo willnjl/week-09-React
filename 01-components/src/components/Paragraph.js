5bdd62b918c5345a674a10e71c1b972d72a3579b
Will <will_leighton@outlook.com>
2020-08-28 11:56:16 +0100

    pre deploy commit

--- 01-components/src/components/Paragraph.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from "react";
-
-const Paragraph = ({ children }) => {
-	return <p>{children ? children : "Hello World"}</p>;
-};
-
-export default Paragraph;
--- 01-components/src/components/routing/FourOhFor.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function FourOhFor() {
-	return (
-		<div className="jumbotron">
-			<h1>404 shits gone wrong</h1>
-		</div>
-	);
-}
